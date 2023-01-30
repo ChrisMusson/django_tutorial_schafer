7b334e1ef9a44bf063526bb2b69e51b738937de2
Chris Musson <chris.musson@hotmail.com>
2023-01-30 21:42:36 +0000

    Ep. 2

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [path("", views.home, name="blog_home")]
+urlpatterns = [
+    path("", views.home, name="blog_home"),
+    path("about/", views.about, name="blog_about"),
+]
--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,8 @@ from django.http import HttpRequest, HttpResponse
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("<h1>This is the home</h1>")
+    return HttpResponse("<h1>This is the BLOG home</h1>")
+
+
+def about(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("<h1>Blog ABOUT</h1>")
