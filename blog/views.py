94f080ddcc3c77d833cc112e19ac1d49efcd82df
Chris Musson <chris.musson@hotmail.com>
2023-01-31 10:16:29 +0000

    Ep. 3

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
+posts = [
+    {
+        "author": "Chris",
+        "title": "First post",
+        "content": "some content here",
+        "date_posted": "27 Aug 2022",
+    },
+    {
+        "author": "Ez",
+        "title": "second post",
+        "content": "some more content here",
+        "date_posted": "29 Aug 2022",
+    },
+]
+
 
 def home(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("<h1>This is the BLOG home</h1>")
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context=context)
 
 
 def about(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("<h1>Blog ABOUT</h1>")
+    return render(request, "blog/about.html", {"title": "About"})
