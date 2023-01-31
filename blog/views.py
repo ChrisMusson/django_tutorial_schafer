ef5ef2dbce69d045a88af3cdf93a3f63590aa9f3
Chris Musson <chris.musson@hotmail.com>
2023-01-31 14:58:58 +0000

    Ep. 5

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
-
-posts = [
-    {
-        "author": "Chris",
-        "title": "First post",
-        "content": "some content here",
-        "date_posted": "27 Aug 2022",
-    },
-    {
-        "author": "Ez",
-        "title": "second post",
-        "content": "some more content here",
-        "date_posted": "29 Aug 2022",
-    },
-]
+from .models import Post
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    context = {"posts": posts}
+    context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context=context)
 
 
